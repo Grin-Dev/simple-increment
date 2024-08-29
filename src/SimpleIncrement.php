67680ae82c790be9d0bd59b109850ca0788de16a
VladimirGrinko <green.devoloper@gmail.com>
2024-08-29 09:52:27 +0300

    Add SimpleIncrement

---
+++ src/SimpleIncrement.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VladimirGrinko\SimpleIncrement;
+
+class SimpleIncrement
+{
+    public function main(int $value): int
+    {
+        return $value++;
+    }
+}
